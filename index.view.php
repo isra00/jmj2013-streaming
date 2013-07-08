e0dd0cd4e6480068397f9fdc74c973088eb4bb83
root <root@ezequiel.rmater.org.br>
2013-07-08 09:56:51 -0300

    Add lang attribute to <html>

--- index.view.php
+++ index.view.php
@@ -2,7 +2,7 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="<?php echo $current_lang ?>"> <!--<![endif]-->
 <head>
     <meta charset="utf-8">
     <title><?php echo $msg['page_title'] ?></title>
