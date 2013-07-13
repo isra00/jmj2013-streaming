4a6fc891b7d6f7cd2aff245f7ecdd72a7e9ebbde
Isra <isra00@gmail.com>
2013-07-13 03:54:05 +0200

    Final PT translation

--- lang-pt.php
+++ lang-pt.php
@@ -1,23 +1,23 @@
 <?php
 
-$msg['page_title']		= 'Encontro vocacional do Caminho Neocatecumenal – retransmissão em vivo';
-$msg['meta_content']	= 'Retransmissão em vivo do encontro nvocacional de jovens do Caminho Neocatecumenal com motivo da JMJ Rio 2013. Kiko Argüello.';
+$msg['page_title']		= 'Encontro vocacional do Caminho Neocatecumenal – ao Vivo';
+$msg['meta_content']	= 'Transmissão ao vivo do encontro vocacional de jovens do Caminho Neocatecumenal por ocasião da JMJ 2013. Rio de Janeiro. Kiko Arguello.';
 $msg['meta_site_name']	= 'Rio 2013 Caminho Neocatecumenal';
 
-$msg['chrome_frame']	= 'Está a usar um navegador <strong>desatualizado</strong>. Por favor, <a href="http://browsehappy.com/">atualiza o teu navegador</a> ou <a href="http://www.google.com/chromeframe/?redirect=true">ativa Google Chrome Frame</a> para melhorá-lo.';
+$msg['chrome_frame']	= 'O seu navegador está <strong>desuatualizado</strong>. Por favor, <a href="http://browsehappy.com/">atualize o seu navegador</a> ou <a href="http://www.google.com/chromeframe/?redirect=true">ative o Google Chrome Frame</a> para melhorá-lo.';
 
 $msg['title']			= 'Encontro vocacional do Caminho Neocatecumenal';
 $msg['subtitle']		= 'JMJ Rio de Janeiro 2013 · ';
-$msg['subtitle_start']	= '29 de julho';
-$msg['intro_text']		= 'Veja em vivo o <strong>encontro de jovens</strong> no Brasil Kiko Argüello, Carmen Hernández e Pe. Mario Pezzi, iniciadores do Caminho Neocatecumenal com motivo da Jornada Mundial da Juventude.';
+$msg['subtitle_start']	= '29 de Julho';
+$msg['intro_text']		= 'Assista ao vivo o <strong>encontro de jovens</strong> no Brasil com Kiko Arguello, Carmem Hernández e Pe. Mario Pezzi, iniciadores do Caminho Neocatecumenal.';
 
-$msg['streaming_help']	= '¿Não pode ver o vídeo corretamente? <a href="">Click aquí</a>.';
+$msg['streaming_help']	= 'Está tendo problemas para asistir o vídeo? <a href="">Clique aquí</a>.';
 
-$msg['not_yet_title']	= 'O evento será retransmitido o 29 de julho ás 14:00 hora de Rio de Janeiro (GMT -3)';
-$msg['local_time']		= 'No seu horário local serão as ';
-$msg['finished']		= 'O encontro já finalizou';
-$msg['unavailable']		= 'A retransmissão não está disponível neste momento. Desculpe as molestias.';
+$msg['not_yet_title']	= 'O evento será transmitido no dia 29 de julho às 14 horas, horário de Brasília (GMT-3)';
+$msg['local_time']		= 'Em seu horário local serão ';
+$msg['finished']		= 'O encontro já terminou';
+$msg['unavailable']		= 'A transmissão não está disponível no momento. Perdoe-nos o transtorno.';
 
 $msg['link_jmj_cnc']	= 'Voltar ao site <strong>Rio 2013 CNC</strong>';
-$msg['link_jmj']		= 'Site oficial <strong>JMJ Rio 2013</strong>';
-$msg['link_cnc'] 		= 'Site oficial <strong>Caminho Neocatecumenal</strong>';
+$msg['link_jmj']		= 'Site Oficial <strong>JMJ Rio 2013</strong>';
+$msg['link_cnc'] 		= 'Site Oficial <strong>Caminho Neocatecumenal</strong>';
